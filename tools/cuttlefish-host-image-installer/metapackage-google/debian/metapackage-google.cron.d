43b502115df28f2a40366880d6566668b7caed75
Wei-chung Hsu <weihsu@google.com>
2024-10-07 14:29:27 -0700

    Two sample codes for Wattson project, action item: cronjob to set AllowedCPUs(wei), may require some prework to assign UIDs.

--- tools/cuttlefish-host-image-installer/metapackage-google/debian/metapackage-google.cron.d
+++ tools/cuttlefish-host-image-installer/metapackage-google/debian/metapackage-google.cron.d
@@ -4,3 +4,4 @@
 # Workarounds for Network-Manager
 @reboot root sleep 100 && /usr/sbin/dhclient
 @reboot root sleep 150 && su vsoc-01 -c '/bin/bash /usr/share/metapackage-google/cf_docker_co_run.sh'
+@reboot root sleep 200 && systemctl set-property user-1000.slice AllowedCPUs=0-11 && systemctl set-property user-1001.slice AllowedCPUs=12-23 && systemctl set-property user-1002.slice AllowedCPUs=24-35 && systemctl set-property user-1003.slice AllowedCPUs=36-47 && systemctl set-property user-1004.slice AllowedCPUs=48-59 && systemctl set-property user-1005.slice AllowedCPUs=60-71 && systemctl set-property user-1006.slice AllowedCPUs=72-83 && systemctl set-property user-1007.slice AllowedCPUs=84-95 && systemctl set-property user-1008.slice AllowedCPUs=96-107 && systemctl set-property user-1009.slice AllowedCPUs=108-119
--- tools/cuttlefish-host-image-installer/metapackage-google/usr/share/cf_docker_co_run.sh
+++ tools/cuttlefish-host-image-installer/metapackage-google/usr/share/cf_docker_co_run.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-# Step 1. Build Cuttlefish docker image if there is no such one
+# Step 1. check if we need to restrict cpus per user
+total_cpu=$(nproc --all)
+counter=0
+uid=1000
+cpus_per_instance=12
+upper_bound=$[$cpus_per_instance -1]
+while [ "$total_cpu" -gt "$upper_bound" ]
+do
+  #systemctl set-property user-1000.slice AllowedCPUs=0-11
+  systemctl set-property user-$uid.slice AllowedCPUs=$counter-$upper_bound
+  counter=$[$counter +$cpus_per_instance]
+  upper_bound=$[$upper_bound +$cpus_per_instance]
+  uid=$[$uid +1]
+done
+
+# Step 2. Build Cuttlefish docker image if there is no such one
 cf_image=$(docker image list | grep "cuttlefish-orchestration")
 if [ "$cf_image" == "" ]; then
   # Build CF docker image
@@ -14,7 +29,7 @@ if [ "$cf_image" == "" ]; then
   /bin/bash image-builder.sh &> build.log
 fi
 
-# Step 2. Run CO server
+# Step 3. Run CO server
 # use the fixed version instead of HEAD, makes it easier to triage problems when they arise
 CO_VERSION="0.1.0-alpha"
 cd /home/vsoc-01
